8242007ac54ce6556a427c6a90935fd6fa89a131
awesomeroks <30774147+praveenVnktsh@users.noreply.github.com>
2022-01-10 21:32:36 +0530

    ..

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -27,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        extendBodyBehindAppBar: true,
         drawer: Drawer(
           elevation: 0,
           child: ListView(
@@ -36,13 +39,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     backgroundColor: Colors.white,
                     child: Text(
                       fullname.substring(0, 1),
-                      style: TextStyle(fontSize: 35, color: Colors.black87),
+                      style:
+                          const TextStyle(fontSize: 35, color: Colors.black87),
                     ),
                   ),
                   accountName: Text(fullname,
-                      style: TextStyle(fontSize: 20, color: Colors.white)),
+                      style:
+                          const TextStyle(fontSize: 20, color: Colors.white)),
                   accountEmail: Text(email,
-                      style: TextStyle(fontSize: 15, color: Colors.white))),
+                      style:
+                          const TextStyle(fontSize: 15, color: Colors.white))),
               ListTile(
                 title: const Text(
                   "Past Orders",
@@ -95,16 +101,84 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         backgroundColor: Colors.white,
         appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Food Ordering App",
-              style: TextStyle(fontSize: 18, color: Colors.white)),
-          elevation: 1,
+          leading: const Icon(
+            Icons.hourglass_bottom,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          // title: Text("SDFSDf"),
+          // centerTitle: true,
         ),
-        body: Container(),
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                gradient: RadialGradient(
+                    colors: [
+                      // Color(0xf0000000),
+                      Color(0xFF73cdbb),
+                      Color(0xFF1ca3a6),
+                    ],
+                    radius: 1.3,
+                    center: Alignment(-1, 0),
+                    stops: [0.0, 1.0],
+                    tileMode: TileMode.clamp),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    Text("Hungry? Delicious food is just a tap away.",
+                        style: GoogleFonts.raleway(
+                          fontWeight: FontWeight.bold,
+                          // fontStyle: FontStyle.italic,
+                          height: 1.5,
+                          color: Colors.white,
+                          fontSize: 23,
+                        )),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: "Search for items",
+                          hintStyle: GoogleFonts.raleway(
+                              // fontStyle: FontStyle.italic,
+                              ),
+                          prefixIcon: const Icon(Icons.search),
+                          border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 0.0),
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                  ],
+                ),
+              ),
+              height: 1000,
+              width: 500,
+            ),
+            SlidingUpPanel(
+                maxHeight: 610.0,
+                minHeight: 500.0,
+                parallaxEnabled: true,
+                parallaxOffset: .1,
+                panel: Container(),
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(18.0),
+                    topRight: Radius.circular(18.0))),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           mini: true,
-          child: Container(child: Icon(Icons.add_shopping_cart)),
+          child: Container(child: const Icon(Icons.add_shopping_cart)),
           elevation: 1,
           backgroundColor: Colors.white,
           foregroundColor: Colors.orange,
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'home.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
